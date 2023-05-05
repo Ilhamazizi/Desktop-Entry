d11d372044edf6d29c87861e74347e82db55bf60
Ilhamazizi <muh.ilhamazizi@gmail.com>
2023-05-05 03:09:53 +0200

    execution

--- .execute.sh
+++ .execute.sh
@@ -1,7 +1,4 @@
 #!/bin/bash 
-sudo mkdir /usr/local/share/application \
-wget https://raw.githubusercontent.com/Ilhamazizi/Desktop-Entry/main/code.desktop && sudo mv code.desktop /usr/local/share/application \
-
 exec=/usr/local/share/application/* \
 bash=/usr/local/share/application/* \
 
