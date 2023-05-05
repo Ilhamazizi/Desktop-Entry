000ff3292ecea5eedb0cd903bafde42e55bd5171
Ilhamazizi <muh.ilhamazizi@gmail.com>
2023-05-05 03:28:52 +0200

    execution

--- .execute.sh
+++ .execute.sh
@@ -2,6 +2,8 @@
 sudo mkdir /usr/local/share/application \
 sudo mv code.desktop firefox.desktop mendeley.desktop thunderbird.desktop packettracer.desktop /usr/local/share/application \
 
-exec=/usr/local/share/application/* && bash=/usr/local/share/application/* \
+exec=/usr/local/share/application/* \
+bash=/usr/local/share/application/* \
+
 echo 'success'
 
