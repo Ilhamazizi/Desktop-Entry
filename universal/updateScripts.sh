600c44be3f39fa546d9aa64478b691aeb9fdb212
Ilhamazizi <muh.ilhamazizi@gmail.com>
2023-05-15 20:41:00 +0200

    add function

--- universal/updateScripts.sh
+++ universal/updateScripts.sh
@@ -3,59 +3,51 @@
 release_file=/etc/os-release
 
 #default logfile : /var/log/*
+#errorfile=/var/log/update_error.log
+#logfile=/var/log/updater.log
 errorfile=${HOME}/.log/updater_error.log
 logfile=${HOME}/.log/updater.log
+
+check_exit_status() {
+    if [ $? -eq 0 ] 
+    then
+        echo "An error occurred, please check the $errorlog file."
+    fi
+}
 
 if grep -q "Arch" $release_file
 then
     # The host is based on Arch, run the pacman update command
     sudo pacman -Syu 1>>$logfile 2>>$errorfile
-    if [ $? -ne 0 ]
-    then
-        echo "An error occured, please check the $errorlog file."
-    fi
+    check_exit_status
 fi
 
 if grep -q "Debian" $release_file || grep -q "Ubuntu" $release_file
 then
     # The host is based on Debian & Ubuntu, run the apt version of the command
     sudo apt update
-    if [ $? -ne 0 ]
-    then
-        echo "An error occured, please check the $errorlog file."
-    fi
+    check_exit_status
+
     sudo apt dist-upgrade
-    if [ $? -ne 0 ]
-    then
-        echo "An error occured, please check the $errorlog file."
-    fi
+    check_exit_status
 fi
 
 if grep -q "openSUSE" $release_file
 then
     # The host is based on openSUSE, run the zypper refresh command
     sudo zypper ref
-    if [ $? -ne 0 ]
-    then
-        echo "An error occured, please check the $errorlog file."
-    fi
+    check_exit_status
+
     sudo zypper up
-    if [ $? -ne 0 ]
-    then
-        echo "An error occured, please check the $errorlog file."
-    fi
+    check_exit_status
 fi
 
 if grep -q "Red Hat" $release_file || grep -q "CentOS" $release_file
 then
    # The host is based on RedHat, run the yum update command
-   sudo yum update
-   if [ $? -ne 0 ]
-    then
-        echo "An error occured, please check the $errorlog file."
-    fi
-    if [ $? -ne 0 ]
-    then
-        echo "An error occured, please check the $errorlog file."
-    fi
+    sudo yum update
+    check_exit_status
+
+    sudo yum upgrade
+    check_exit_status
 fi
