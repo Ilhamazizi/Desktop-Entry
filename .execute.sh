7ac72a3e57289f948baa78c5965169f98fe359f6
Ilhamazizi <muh.ilhamazizi@gmail.com>
2023-05-16 14:37:40 +0200

    execution

--- .execute.sh
+++ .execute.sh
@@ -1,9 +1,9 @@
-#!/bin/bash 
-sudo mkdir /usr/local/share/application \
-sudo mv code.desktop firefox.desktop mendeley.desktop thunderbird.desktop packettracer.desktop mongodb-compass.desktop /usr/local/share/application \
+#!/bin/bash
+sudo mkdir /usr/local/share/applications \
+sudo mv code.desktop firefox.desktop mendeley.desktop thunderbird.desktop packettracer.desktop mongodb-compass.desktop /usr/local/share/applications \
 
-exec=/usr/local/share/application/* \
-bash=/usr/local/share/application/* \
+exec=/usr/local/share/applications/* \
+bash=/usr/local/share/applications/* \
 
 echo 'success'
 
