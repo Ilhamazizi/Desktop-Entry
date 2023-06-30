24bfd6142a475ac59dcd62f06ffe886e0460e087
Muhammad Ilham Azizi <rudolph@azeezee.net>
2023-06-30 14:08:57 +0000

    firefox

--- .execute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo mkdir /usr/local/share/applications \
-sudo mv code.desktop firefox.desktop mendeley.desktop thunderbird.desktop packettracer.desktop mongodb-compass.desktop /usr/local/share/applications \
-
-exec=/usr/local/share/applications/* \
-bash=/usr/local/share/applications/* \
-
-echo 'success'
-
